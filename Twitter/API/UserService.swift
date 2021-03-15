6eee9739709137e168ae00491a494ed6e771dc08
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-03-15 10:45:14 +0300

    User feed navigation bar

---
+++ Twitter/API/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  Twitter
+//
+//  Created by Александр Кисляков on 15.03.2021.
+//
+
+import Foundation
---
+++ Twitter/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by Александр Кисляков on 15.03.2021.
+//
+
+import Foundation
